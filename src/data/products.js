2ec00c686bbdd42de1f99cd91fdd0025b5820a6c
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-02-17 16:40:12 +0600

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Ohrensessel Josslyn',
+    price: 4099.99,
+    currency: 'BD',
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'OhrenSessel Large Sofie',
+    price: 2049.99,
+    currency: 'BD',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Schlafsessel Rovigo',
+    price: 2039.99,
+    currency: 'BD',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Sessel Little',
+    price: 1019.99,
+    currency: 'EUR',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sessel Peacock',
+    price: 5099.99,
+    currency: 'BD',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sessel Anna',
+    price: 1049.99,
+    currency: 'BD',
+    image: 'images/03.jpg',
+  },
+];
+
+export default data;
